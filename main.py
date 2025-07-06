baf3670b49c132f72d63bda93759be2ec2fe1564
DharmX1 <101920887+DharmX1@users.noreply.github.com>
2025-07-06 17:50:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -376,9 +376,9 @@ async def start(bot, m: Message):
             f"Use Command : /help to get started 🌟\n\n"
             f"If you face any problem contact -  [{CREDIT}](https://t.me/saini_contact_bot)\n", disable_web_page_preview=True, reply_markup=BUTTONSCONTACT
         )
-    else:
-        await asyncio.sleep(2)
-        await start_message.edit_text(
+    
+   await asyncio.sleep(2)
+   await start_message.edit_text(
            f" 🎉 Welcome {m.from_user.first_name} to DRM Bot! 🎉\n\n"
            f"You can have access to download all Non-DRM+AES Encrypted URLs 🔐 including\n\n"
            f"Use Command : /help to get started 🌟\n\n"
